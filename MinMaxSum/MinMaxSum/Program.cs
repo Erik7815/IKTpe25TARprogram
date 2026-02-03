1072c8cbe08d0aa65426cac47485641bd13ce9d7
ssigridss-dot <ssigridss@gmail.com>
2026-02-03 15:45:35 +0200

    number

---
+++ MinMaxSum/MinMaxSum/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MinMaxSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List numbrites");
+
+            int[] numbers = new int [10] { 2, 12, 15, 11, 6, 9, 41, 47, 5, 67 };
+
+            Console.WriteLine(numbers.Max());
+            Console.WriteLine(numbers.Min());
+            Console.WriteLine(numbers.Sum());
+            Console.WriteLine(numbers.Average());
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Sorteerib numbrid alates väiksemast suuremani");
+
+            Array.Sort(numbers);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("");
+
+            //sorteerib numbrid alates suuremast väiksemani
+            Array.Reverse(numbers);
+            foreach (int j in numbers)
+            {
+                Console.WriteLine(j);
+                
+            }
+        }
+    }
+}
