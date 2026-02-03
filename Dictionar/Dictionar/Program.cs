027aa8a027ffefb4b5c33c69d6fd6b250d5260aa
ssigridss-dot <ssigridss@gmail.com>
2026-02-03 15:14:19 +0200

    dic

---
+++ Dictionar/Dictionar/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Dictionar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dictionary ja foreach!");
+
+            // var on muutuja 
+            int nr = 1;
+            var domains = new Dictionary<string, string>()
+            {
+                {"fi", "Finland" },
+                {"se", "Sweden" },
+                {"de", "Germany" },
+                {"fr", "France" },
+                {"es", "Spain" }
+            };
+            //kasutate foreach-i ja juurde tuleb lisada kolmas rida
+            foreach (var domain in domains)
+            {
+                Console.WriteLine($"{domain.Key} - {domain.Value} - {nr}");
+                nr++;
+            }
+        }
+    }
+}
