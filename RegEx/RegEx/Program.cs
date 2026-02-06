85c38e62b11422239bd0c23bc1e868b94751f4dd
Opilane TTHK <opilane@tthk.ee>
2026-02-06 10:07:32 +0200

    regex

---
+++ RegEx/RegEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teeme regular expresion harjutuse");
+
+            string word = "#CD5C5Cy";
+            Console.WriteLine("Hex code: " + word);
+            Console.WriteLine("kas on regex: " + Regextest(word));
+        }
+        public static bool Regextest(string word)
+        {
+            //regular expression kontrollib kas sisestatav string vastab nõuetele
+            return Regex.IsMatch(word, @"[#][0-9A-Fa-f]{6}\b");
+        }
+    }
+}
