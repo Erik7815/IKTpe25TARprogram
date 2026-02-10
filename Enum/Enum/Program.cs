f2522d6aca40f6eae7c3a151db8e4ffecafe5350
Opilane TTHK <opilane@tthk.ee>
2026-02-10 15:09:56 +0200

    enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Channels;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enum");
+            Console.WriteLine(Weekdays.Friday);
+            int day = (int)DayOfWeek.Friday;
+            Console.WriteLine(day);
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine(colors.White);
+            
+        }
+        enum Weekdays
+        {
+            Monday, 
+            Tuesday, 
+            Wednesday, 
+            Thursday, 
+            Friday, 
+            Saturday, 
+            Sunday
+        }
+        enum colors
+        {
+            Red = 3,
+            Green = 10,
+            Blue = 13,
+            Yellow = 5,
+            Black = 1,
+            White = 8,
+
+        }
+    }
+}
