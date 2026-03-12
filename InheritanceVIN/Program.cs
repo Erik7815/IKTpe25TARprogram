72db176ae71784d969f21f549dcf70b1f39d35f8
Opilane TTHK <opilane@tthk.ee>
2026-03-12 11:31:50 +0200

    vin

---
+++ InheritanceVIN/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace InheritanceVIN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masina number");
+            int vincode = Convert.ToInt32(Console.ReadLine());
+            Machine machine = new Machine();
+            machine.Vinkood(vincode);
+            Console.WriteLine("VIN kood: " + machine.Vinkood);
+        }
+    }
+    class Machine : Cars
+    {
+        public int masinanumber()
+        {
+            return vin;
+        }
+    }
+    class Cars
+    {
+       public void Vinkood(int vincode)
+        {
+            vin = vincode;
+        }
+            
+        protected int vin;
+    }
+    
+}
